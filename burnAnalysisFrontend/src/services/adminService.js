a61e9297c674988b3beed4ce13013f4a70ecd618
Idil Nil Zemheri <138678458+idilnil@users.noreply.github.com>
2025-06-02 13:57:01 +0300

    new

--- burnAnalysisFrontend/src/services/adminService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const getAdminInfo = async () => {
-  const token = localStorage.getItem("token"); // Admin giriş token'ı
-  return await axios.get("http://localhost:5005/api/admin/info", {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-};
